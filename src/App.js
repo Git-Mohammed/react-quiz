23bc54779b20b809f477d9970d00b1679b91cb85
Mohammed Saif <xmohammedfares@gmail.com>
2026-08-02 22:40:34 +0300

    start the react quiz app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,70 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
 
-export default function App() {
+//"loading", "error"
+const initialState = { questions: [], status: "loading" };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecevied":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailded":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "startQuiz":
+      return {
+        ...state,
+        status: "active",
+      };
+    default:
+      throw new Error("Action unkown");
+  }
+}
+
+function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+
+  function handleOnClick() {
+    dispatch({ type: "startQuiz" });
+  }
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const resp = await fetch("http://localhost:4000/questions");
+        const data = await resp.json();
+        dispatch({ type: "dataRecevied", payload: data });
+      } catch (err) {
+        console.log(err);
+        dispatch({ type: "dataFailded" });
+      }
+    }
+    fetchQuestions();
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} onClick={handleOnClick} />
+        )}
+        {status === "active" && <Question />}
+      </Main>
     </div>
   );
 }
+
+export default App;
---
+++ src/StartScreen.js
@@ -0,0 +1,13 @@
+function StartScreen({ numQuestions, onClick }) {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz!</h2>
+      <h3>{numQuestions} question to teest your React mastrery</h3>
+      <button className="btn btn-ui" onClick={onClick}>
+        Let's Start
+      </button>
+    </div>
+  );
+}
+
+export default StartScreen;
